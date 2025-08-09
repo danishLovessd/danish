f350b1ecf2f1ddf53109787c6c52c25a4dfdd0d7
danishLovessd <danishalambth@gmail.com>
2025-08-09 21:12:47 +0530

    Add files via upload

--- script.js
+++
@@ -1,9 +0,0 @@
-function showSignup() {
-  document.getElementById("loginForm").classList.remove("active");
-  document.getElementById("signupForm").classList.add("active");
-}
-
-function showLogin() {
-  document.getElementById("signupForm").classList.remove("active");
-  document.getElementById("loginForm").classList.add("active");
-}
